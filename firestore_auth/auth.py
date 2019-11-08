f0df29c3748f94f6eb03d67e67db88166f84cfea
Ryan Chen <ryanjchen2@gmail.com>
2019-11-08 12:31:11 -0500

    Exposing the authenticate function

--- firestore_auth/auth.py
+++ firestore_auth/auth.py
@@ -4,22 +4,29 @@ from google.cloud import firestore
 
 
 def authenticate(
-    username: str, password: str, collection: str = "users"
+    username: str,
+    password: str,
+    collection: str = "users",
+    db: firestore.Client = None,
 ) -> bool:
     """A function to authenticate with a Firestore database.
-    
     Args:
         username (str): The requester's username.
         password (str): The requester's password.
         collection (str): Keyword argument. Represents the
             the name of the collection to query.
+        db (google.cloud.firestore.Client): Keyword argument. A custom client
+            that can be used for testing.
 
     Returns:
         bool: True if successful, False otherwise.
 
     :ret type: bool
     """
-    db = firestore.Client()
+
+    if db is None:
+        db = firestore.Client()
+
     user_ref = (
         db.collection(collection).where("username", "==", username).stream()
     )

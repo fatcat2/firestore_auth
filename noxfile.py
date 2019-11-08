18fa108e43d5dead1df7b2dfa8ba540a3a737231
Ryan Chen <ryanjchen2@gmail.com>
2019-11-08 12:30:05 -0500

    Added nox for linting and testing

---
+++ noxfile.py
@@ -0,0 +1,46 @@
+from __future__ import absolute_import
+import nox
+
+BLACK_PATHS = [
+        "firestore_auth",
+        "tests"
+]
+
+
+@nox.session(python="3.7")
+def lint(session):
+    """Run linters.
+    Returns a failure if the linters find linting errors or sufficiently
+    sersious code quality issues.
+    """
+    session.install("flake8", "black")
+    session.install("-r", "requirements.txt")
+    session.run("black", "--check", *BLACK_PATHS)
+    session.run("flake8", *BLACK_PATHS)
+
+
+@nox.session(python="3.7")
+def blacken(session):
+    """Run black.
+    """
+    session.install("black")
+    session.run("black", *BLACK_PATHS)
+
+
+def default(session):
+    # Install all test dependencies, then isntall this package in-place.
+    session.install("mock", "pytest", "pytest-cov", "mock-firestore")
+    session.install("-r", "requirements.txt")
+    session.install("-e", ".")
+    session.run(
+            "py.test",
+            "--quiet",
+            "--cov=firestore_auth",
+            "--cov-fail-under=0",
+            *session.posargs
+    )
+
+
+@nox.session(python=["3.7"])
+def unit(session):
+    default(session)
